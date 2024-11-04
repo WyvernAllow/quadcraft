1738de1364e92a6d18f2523821ac7430ff995610
WyvernAllow <bloodflame2535@gmail.com>
2024-11-04 09:16:04 +0200

    window

--- src/main.c
+++ src/main.c
@@ -1,5 +1,32 @@
-#include <stdio.h>
+#include <SDL3/SDL.h>
+#include <stdlib.h>
 
-int main(int, char**){
-    printf("Hello, from quadcraft!\n");
+int main() {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        SDL_Log("SDL_Init failed: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window *window =
+        SDL_CreateWindow("Quadcraft", 800, 450, SDL_WINDOW_RESIZABLE);
+    if (!window) {
+        SDL_Log("SDL_CreateWindow failed: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    bool is_running = true;
+    while (is_running) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e)) {
+            switch (e.type) {
+            case SDL_EVENT_QUIT:
+                is_running = false;
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return EXIT_SUCCESS;
 }
