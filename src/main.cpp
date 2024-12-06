e992c122efbbafe78a3df84e875c6ca241aa3a5d
WyvernAllow <bloodflame2535@gmail.com>
2024-12-06 19:16:15 +0200

    window

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,38 @@
-#include <iostream>
+#include <spdlog/spdlog.h>
+
+#include <cstdlib>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vk_enum_string_helper.h>
+#include <vulkan/vulkan.h>
+#include <vulkan/vulkan_core.h>
+
+static void glfw_error_callback(int error_code, const char* description) {
+    spdlog::error("{}", description);
+}
 
 int main() {
-    std::cout << "Hello, World!\n";
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) {
+        spdlog::critical("glfwInit failed");
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    GLFWwindow* window =
+        glfwCreateWindow(800, 450, "Quadcraft", nullptr, nullptr);
+    if (!window) {
+        spdlog::critical("glfwCreateWindow failed");
+        return EXIT_FAILURE;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
