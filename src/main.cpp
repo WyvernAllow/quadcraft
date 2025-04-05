900b41faa7543756a1e346c48c1f756f8fc9b6e7
WyvernAllow <bloodflame2535@gmail.com>
2025-04-05 21:22:23 +0200

    Window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,84 @@
+#include <cstdlib>
 #include <iostream>
 
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+#include <glad/gl.h>
+#include <spdlog/spdlog.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+static void glfw_error_callback(int error, const char* description) {
+    spdlog::error("GLFW: {}", description);
+}
+
+static void gl_debug_callback(GLenum source, GLenum type, GLuint id,
+                              GLenum severity, GLsizei length,
+                              const GLchar* message, const void* userParam) {
+    switch (severity) {
+    case GL_DEBUG_SEVERITY_HIGH:
+        spdlog::error("OpenGL: {}", message);
+        break;
+    case GL_DEBUG_SEVERITY_MEDIUM:
+        spdlog::warn("OpenGL: {}", message);
+        break;
+    case GL_DEBUG_SEVERITY_LOW:
+        spdlog::error("OpenGL: {}", message);
+        break;
+    default:
+        spdlog::debug("OpenGL: {}", message);
+        break;
+    }
+}
+
+static void glfw_framebuffer_size_callback(GLFWwindow* window, int width,
+                                           int height) {
+    glViewport(0, 0, width, height);
+}
+
 int main() {
-    std::cout << "Hello, World!\n";
-    return 0;
+    glfwSetErrorCallback(glfw_error_callback);
+    if (!glfwInit()) {
+        spdlog::error("glfwInit failed");
+        return EXIT_FAILURE;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GL_TRUE);
+    GLFWwindow* window =
+        glfwCreateWindow(800, 450, "Quadcraft", nullptr, nullptr);
+    if (!window) {
+        spdlog::error("glfwCreateWindow failed");
+        return EXIT_FAILURE;
+    }
+
+    glfwSetFramebufferSizeCallback(window, glfw_framebuffer_size_callback);
+
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGL(glfwGetProcAddress)) {
+        spdlog::error("gladLoadGL failed");
+        return EXIT_FAILURE;
+    }
+
+    glEnable(GL_DEBUG_OUTPUT);
+    glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+    glDebugMessageCallback(gl_debug_callback, nullptr);
+    glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr,
+                          GL_TRUE);
+
+    while (!glfwWindowShouldClose(window)) {
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
